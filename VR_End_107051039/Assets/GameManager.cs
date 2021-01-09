983d9e399fe79b5cd1228b352bdd32aee6de14ad
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2021-01-09 15:16:17 +0800

    除了分數問題以外都好了

---
+++ VR_End_107051039/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "布偶") 
+        {
+            print("一號燈爆掉");
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VR場景");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
